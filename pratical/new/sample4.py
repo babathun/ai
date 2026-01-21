fdccd4062934fc723457189556709f20c6cb2353
babathun <itzmevaishnav@gmail.com>
2026-01-21 06:25:19 +0530

    Update sample4.py

--- pratical/new/sample4.py
+++ pratical/new/sample4.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
-img = cv2.imread('image.jpg')
+img = cv2.imread(r'C:\users\Admin\Desktop\pratical\new\image.jpg')
+
 plt.imshow(img)
 plt.title('car')
 plt.axis('off')
